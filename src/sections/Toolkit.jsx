154c33725522d49cf8403735512585dc8468e33a
5ee5 <marks.sislovs@gmail.com>
2025-11-10 22:13:30 +0200

    Add React to the Toolkit

--- src/sections/Toolkit.jsx
+++ src/sections/Toolkit.jsx
@@ -5,6 +5,7 @@ export default function Toolkit() {
       <ul>
         <li><i className="fa-solid fa-code"></i> Neovim</li>
         <li><i className="fa-brands fa-html5"></i> HTML CSS JS</li>
+	<li><i className="fa-brands fa-react"></i> React</li>
         <li><i className="fa-brands fa-python"></i> Python</li>
         <li><i className="fa-brands fa-linux"></i> Arch Linux</li>
       </ul>
