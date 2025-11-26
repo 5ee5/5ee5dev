a9407988af2110468f9a3feb0077a358f072ebcb
5ee5 <marks.sislovs@gmail.com>
2025-11-26 16:18:29 +0200

    Add Tech section

---
+++ src/sections/Tech.jsx
@@ -0,0 +1,11 @@
+export default function Tech() {
+  return (
+    <section id="tools-section">
+      <h2>Tech</h2>
+      <ul>
+        <li><i className="fa-solid fa-laptop"></i> Lenovo Ideapad Gaming 3</li>
+	<li><i class="fa-solid fa-server"></i> Xeon E5-2680 v4</li>
+      </ul>
+    </section>
+  );
+}
--- src/sections/Toolkit.jsx
+++ src/sections/Toolkit.jsx
@@ -4,7 +4,7 @@ export default function Toolkit() {
       <h2>Toolkit</h2>
       <ul>
         <li><i className="fa-brands fa-html5"></i> HTML CSS JS</li>
-	<li><i className="fa-brands fa-react"></i> React</li>
+	<li><i className="fa-brands fa-react fa-spin"></i> React</li>
         <li><i className="fa-brands fa-python"></i> Python</li>
         <li><i className="fa-brands fa-linux"></i> Linux</li>
       </ul>
