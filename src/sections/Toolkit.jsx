8af019b29cbc471ac2763fcc516075eeae8d106a
5ee5 <marks.sislovs@gmail.com>
2025-12-04 13:25:41 +0200

    Adding separate CSS and JS toolkit entries

--- src/sections/Toolkit.jsx
+++ src/sections/Toolkit.jsx
@@ -3,7 +3,9 @@ export default function Toolkit() {
     <section id="tools-section">
       <h2>Toolkit</h2>
       <ul>
-        <li><i className="fa-brands fa-html5 html-icon"></i> HTML CSS JS</li>
+        <li><i className="fa-brands fa-html5 html-icon"></i> HTML</li>
+	<li><i className="fa-brands fa-css3-alt css-icon"></i> CSS</li>
+	<li><i className="fa-brands fa-js js-icon"></i> JS</li>
 	<li><i className="fa-brands fa-react fa-spin react-icon"></i> React</li>
         <li><i className="fa-brands fa-python python-icon"></i> Python</li>
         <li><i className="fa-brands fa-linux linux-icon"></i> Linux</li>
