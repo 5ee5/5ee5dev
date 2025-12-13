79740fbd72cee254e34fa25fc5dbc4859f830108
5ee5 <marks.sislovs@gmail.com>
2025-12-13 20:33:21 +0200

    Add macbook pro to the "Tech" section

--- src/sections/Tech.jsx
+++ src/sections/Tech.jsx
@@ -4,7 +4,8 @@ export default function Tech() {
       <h2>Tech</h2>
       <ul>
         <li><i className="fa-solid fa-laptop"></i> Lenovo Ideapad Gaming 3</li>
-	<li><i class="fa-solid fa-server"></i> Xeon E5-2680 v4</li>
+        <li><i className="fa-solid fa-server"></i> Xeon E5-2680 v4</li>
+        <li><i className="fa-brands fa-apple"></i> MacBook Pro 2016</li>
       </ul>
     </section>
   );
