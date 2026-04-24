fc638624bfe20a6f0c8cb70800911bc50b86fcd8
5ee5 <marks.sislovs@gmail.com>
2026-04-24 19:56:40 +0300

    webring src url fix

--- src/sections/Webring.jsx
+++ src/sections/Webring.jsx
@@ -2,7 +2,7 @@ export default function Webring() {
   return (
     <section id="webring-section">
       <iframe
-        src="https://ring.pre1ude.dev/ring?url=https://5ee5.github.io/5ee5dev/&fgcolor=8B0000&bgcolor=000000"
+        src="https://ring.pre1ude.dev/ring?url=https://5ee5.dev/&fgcolor=8B0000&bgcolor=000000"
         width="230"
         height="100"
         frameBorder="0"
