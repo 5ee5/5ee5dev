a965efdb45cf5952e64b7766d6de969851848927
5ee5 <marks.sislovs@gmail.com>
2026-08-04 19:16:44 +0300

    Removing blinking cursor from about me section

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -3,7 +3,7 @@ export default function AboutMe() {
     <section id="bio">
       <h2>About me</h2>
       <img src="/e5.png" alt="5ee5" className="circle-image" />
-      <ul id="typing-list">
+      <ul id="about-me-list">
         <li><span>Developer</span></li>
         <li><span>Linux Enthusiast</span></li>
         <li><span>Runner</span></li>
